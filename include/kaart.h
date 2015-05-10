438b04cf3b76176995bd5f3d4b47819b92ba6d82
jelmew <jelmervanamen@gmail.com>
2015-05-10 08:02:42 +0200

    Refactored kaart class

--- include/kaart.h
+++ include/kaart.h
@@ -8,39 +8,39 @@
 using namespace std;
 using namespace boost::assign; // bring 'map_list_of()' into scope
 
-
-enum kaartkleur{klaveren, schoppen, harten ,ruiten};
-
-enum kaarttype{aas,twee,drie,vier,vijf,zes,zeven,acht,negen,tien,
-boer,vrouw,heer};
-
-
-class kaart
-{
-	public:
-
-	kaart();
-	kaart(kaartkleur input_kaartkleur,kaarttype input_kaarttype):
-	kleur(input_kaartkleur), type(input_kaarttype)
-	{
-	};
-	kaart(kaart& other):kleur(other.kleur), type(other.type)
-	{};
-	~kaart(){};
-
-	friend std::ostream& operator<<(std::ostream& os, const kaart& print);
-
-	friend bool operator==(kaart& a, kaart& b);
-	friend bool operator!=(kaart& a, kaart& b);
+/** Enum voor kleur kaart**/
+enum kaartkleur {klaveren, schoppen, harten,ruiten};
+/**Enum voor kaarttype**/
+enum kaarttype {aas,twee,drie,vier,vijf,zes,zeven,acht,negen,tien,
+    boer,vrouw,heer
+               };
 
 
+/**Class kaart. Slaat alle gegevens van een kaart op **/
+class kaart {
+  public:
+
+    kaart();
+    kaart(kaartkleur input_kaartkleur,kaarttype input_kaarttype):
+      kleur(input_kaartkleur), type(input_kaarttype) {
+      };
+    kaart(kaart& other):kleur(other.kleur), type(other.type) {
+      };
+    ~kaart() {};
+
+    friend std::ostream& operator<<(std::ostream& os, const kaart& print);
+
+    friend bool operator==(kaart& a, kaart& b);
+    friend bool operator!=(kaart& a, kaart& b);
 
 
-	const kaartkleur kleur;
-	const kaarttype type;
-	protected:
-	private:
-};
+
+
+    const kaartkleur kleur;
+    const kaarttype type;
+  protected:
+  private:
+  };
 
 
 
--- src/kaart.cpp
+++ src/kaart.cpp
@@ -2,45 +2,78 @@
 
 const  map<string, kaartkleur> kleur_map=map_list_of("klaveren",klaveren)("schoppen",schoppen)("harten",harten)("ruiten",ruiten);
 const  map<string,kaarttype> type_map=map_list_of("aas",aas)("twee",twee)("drie",drie)("vier",vier)("vijf",vijf)("zes",zes)("zeven",zeven)
-("acht",acht)("negen",negen)("tien",tien)("boer",boer)("vrouw",vrouw)("heer",heer);
-std::ostream& operator<<(std::ostream& os, const kaart& print)
-{
-	switch(print.kleur){
-		case harten: 		os<<"harten ";		break;
-		case ruiten:		os<<"ruiten ";		break;
-		case schoppen: 	os<<"schoppen ";		break;
-		case klaveren: 	os<<"klaveren ";	break;
-		default:		os.setstate(std::ios_base::failbit);
-		}
-	switch(print.type){
-		case aas: 			os<<"aas";				break;
-		case twee:			os<<"twee";				break;
-		case drie:			os<<"drie";				break;
-		case vier:			os<<"vier";				break;
-		case vijf:			os<<"vijf";				break;
-		case zes:				os<<"zes";				break;
-		case zeven:			os<<"zeven";			break;
-		case acht:			os<<"acht";				break;
-		case negen:			os<<"negen";			break;
-		case tien:			os<<"tien";				break;
-		case boer:			os<<"boer";				break;
-		case vrouw:			os<<"vrouw";			break;
-		case heer:			os<<"heer";				break;
-		default: 				os.setstate(std::ios_base::failbit);
-	}
+    ("acht",acht)("negen",negen)("tien",tien)("boer",boer)("vrouw",vrouw)("heer",heer);
+std::ostream& operator<<(std::ostream& os, const kaart& print) {
+  switch(print.kleur) {
+    case harten:
+      os<<"harten ";
+      break;
+    case ruiten:
+      os<<"ruiten ";
+      break;
+    case schoppen:
+      os<<"schoppen ";
+      break;
+    case klaveren:
+      os<<"klaveren ";
+      break;
+    default:
+      os.setstate(std::ios_base::failbit);
+    }
+  switch(print.type) {
+    case aas:
+      os<<"aas";
+      break;
+    case twee:
+      os<<"twee";
+      break;
+    case drie:
+      os<<"drie";
+      break;
+    case vier:
+      os<<"vier";
+      break;
+    case vijf:
+      os<<"vijf";
+      break;
+    case zes:
+      os<<"zes";
+      break;
+    case zeven:
+      os<<"zeven";
+      break;
+    case acht:
+      os<<"acht";
+      break;
+    case negen:
+      os<<"negen";
+      break;
+    case tien:
+      os<<"tien";
+      break;
+    case boer:
+      os<<"boer";
+      break;
+    case vrouw:
+      os<<"vrouw";
+      break;
+    case heer:
+      os<<"heer";
+      break;
+    default:
+      os.setstate(std::ios_base::failbit);
+    }
 
-	return os;
-};
+  return os;
+  };
 
-bool operator==( kaart& a,  kaart& b)
-{
-	return(a.kleur==b.kleur && a.type==b.type);
+bool operator==( kaart& a,  kaart& b) {
+  return(a.kleur==b.kleur && a.type==b.type);
 
-}
+  }
 
-bool operator!=(kaart& a, kaart& b)
-{
-	return !(a==b);
+bool operator!=(kaart& a, kaart& b) {
+  return !(a==b);
 
-}
+  }
 
